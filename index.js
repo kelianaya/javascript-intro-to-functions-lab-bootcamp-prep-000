e501998e6edc3fc2d437faed991e3b6a5476e4af
Keli Anaya <keli.anaya@gmail.com>
2019-07-08 17:44:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,24 +11,14 @@ function logWhisper(string) {
   console.log("hello")
 }
 
-var uppercase = "HELLO!"
-
-uppercase.toUpperCase() === uppercase
-
-var lowercase = 'hello!'
-
-lowercase.toLowerCase() === lowercase
-
-var mixedCase = 'Hi there!'
-
-mixedCase.toLowerCase() === mixedCase
-
-mixedCase.toUpperCase() === mixedCase
-
-function sayHiToGrandma("I can't hear you!") {
- if (`string` === lowercase) {
- return("I can't hear you!")
- } else if (`string` === uppercase) {
-  return("YES INDEED!")
-} 
+function sayHiToGrandma(string){
+	if (string.toLowerCase() === string){
+	  return 'I can\'t hear you!';
+	}
+	if (string.toUpperCase() === string){
+	  return 'YES INDEED!';
+	}
+	if (string === 'I love you, Grandma.'){
+	  return 'I love you, too.';
+	}
 }
